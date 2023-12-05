ac2b4ddc8f934821da5718dba53bcd9457ba78a0
RCV29 <roco.visperas.up@phinmaed.com>
2023-12-05 21:44:20 +0800

    5th

--- app/src/main/java/com/visperas/rolito/block6/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/visperas/rolito/block6/p1/quiz/MainActivity.kt
@@ -2,7 +2,7 @@ package com.visperas.rolito.block6.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
